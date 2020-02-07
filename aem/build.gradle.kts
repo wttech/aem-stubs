4aed92d08324eb49a01570e15edd009e9d1c5166
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2020-02-07 16:14:47 +0100

    Init, seems to be working...

--- aem/build.gradle.kts
+++ aem/build.gradle.kts
@@ -5,3 +5,22 @@ plugins {
 apply(from = rootProject.file("gradle/common.gradle.kts"))
 
 description = "Wiremock on AEM - AEM"
+aem {
+    tasks {
+        instanceSatisfy {
+            // customizing CRX packages to be deployed as dependencies before built AEM application
+            packages {
+                "jetty.util" {resolve("org.eclipse.jetty:jetty-util:9.4.20.v20190813")}
+                "jetty.servlets" {resolve("org.eclipse.jetty:jetty-io:9.4.20.v20190813")}
+                "jetty.servlets" {resolve("org.eclipse.jetty:jetty-xml:9.4.20.v20190813")}
+                "jetty.servlets" {resolve("org.eclipse.jetty:jetty-security:9.4.20.v20190813")}
+                "jetty.servlets" {resolve("org.eclipse.jetty:jetty-servlets:9.4.20.v20190813")}
+                "jetty.servlet" {resolve("org.eclipse.jetty:jetty-servlet:9.4.20.v20190813")}
+                "jetty.server" {resolve("org.eclipse.jetty:jetty-server:9.4.20.v20190813")}
+                "jetty.webapp" {resolve("org.eclipse.jetty:jetty-webapp:9.4.20.v20190813")}
+                "jetty.http" {resolve("org.eclipse.jetty:jetty-http:9.4.20.v20190813")}
+                "sun.misc" {resolve("net.sdruskat:net.sdruskat.fragment.sun.misc:1.0.0")}
+            }
+        }
+    }
+}
--- aem/core/build.gradle.kts
+++ aem/core/build.gradle.kts
@@ -9,47 +9,36 @@ description = "Wiremock on AEM - Core"
 
 aem {
     tasks {
-        bundleExportEmbed("com.github.tomakehurst:wiremock-jre8:2.26.0", "com.github.tomakehurst.*")
+        bundlePrivateEmbed("com.github.tomakehurst:wiremock-jre8:2.21.0")
         bundlePrivateEmbed("com.google.guava:guava:27.0.1-jre",
+                "com.google.common.base",
+                "com.google.common.base.internal",
                 "com.google.common.collect",
                 "com.google.common.net",
-                "com.google.common.base",
-                "com.google.common.graph",
                 "com.google.common.escape",
                 "com.google.common.hash",
                 "com.google.common.io",
+                "com.google.common.graph",
                 "com.google.common.math",
                 "com.google.common.primitives",
-                "com.google.thirdparty.publicsuffix",
-                "com.google.common.base.internal",
-                "com.google.common.cache",
-                "com.google.common.util.concurrent"
-        )
+                "com.google.thirdparty.publicsuffix")
+
+
+        bundlePrivateEmbed("org.checkerframework:checker-qual:2.11.0",
+                "org.checkerframework.framework.qual;version=2.11.0", "org.checkerframework.checker.nullness.qual")
+        bundlePrivateEmbed("com.google.errorprone:error_prone_parent:2.3.4", "com.google.errorprone.annotations",
+                "com.google.errorprone.annotations.concurrent")
         bundlePrivateEmbed("com.google.guava:failureaccess:1.0.1",
                 "com.google.common.util.concurrent.internal"
         )
 
-        bundlePrivateEmbed("com.google.errorprone:error_prone_parent:2.3.4", "com.google.errorprone.annotations",
-                "com.google.errorprone.annotations.concurrent")
-        bundlePrivateEmbed("org.checkerframework:checker-qual:2.11.0",
-                "org.checkerframework.checker.nullness.qual;version=2.11.0")
-        bundlePrivateEmbed("com.github.jknack:handlebars:4.1.2",
-                "com.github.jknack.handlebars.*",
-                "com.github.jknack.handlebars.internal.*"
-                )
-        bundlePrivateEmbed("com.github.jknack:handlebars-jackson2:4.1.2",
-                "com.github.jknack.handlebars.internal.js"
-        )
-
         bundleCompose {
-            //excludePackage("sun.misc", "org.checkerframework.checker.nullness.qual")
-            importPackage("!net.javacrumbs.jsonunit.core", "!org.xmlunit", "*")
             activator = "com.company.aem.wiremockonaem.aem.activator.Activator"
         }
     }
 }
 
 dependencies {
-    compileOnly("com.github.tomakehurst:wiremock:2.26.0")
+    compileOnly("com.github.tomakehurst:wiremock-jre8:2.21.0")
 
 }
