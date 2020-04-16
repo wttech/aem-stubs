0fae3807226fde5a0e002b34fed0c7c22a9ad507
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2020-04-16 08:00:47 +0200

    add configuration

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.cognifide.aem.bundle")
     id("com.cognifide.aem.package")
+    id("com.cognifide.aem.package.sync")
 }
 
 apply(from = rootProject.file("gradle/common.gradle.kts"))
