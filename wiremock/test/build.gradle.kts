e2c1a2df38b33df92d7fcd483c3c68f1ee01d273
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2020-04-17 16:00:15 +0200

    Handle not supported stubbing

--- wiremock/test/build.gradle.kts
+++ wiremock/test/build.gradle.kts
@@ -7,9 +7,6 @@ plugins {
 apply(from = rootProject.file("gradle/common.gradle.kts"))
 
 dependencies {
-    compileOnly(project(":core"))
-
     testImplementation("io.rest-assured:rest-assured:3.3.0")
     testImplementation("io.rest-assured:json-schema-validator:3.3.0")
-
 }
