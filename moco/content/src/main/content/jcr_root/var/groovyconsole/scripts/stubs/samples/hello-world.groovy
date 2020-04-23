2ec50f82f14df0c5657a7bec38eb671c169372d1
Krystian Panek <krystian.panek@cognifide.com>
2020-04-23 07:37:17 +0200

    Moco dynamic vars support

--- moco/content/src/main/content/jcr_root/var/groovyconsole/scripts/stubs/samples/hello-world.groovy
+++ moco/content/src/main/content/jcr_root/var/groovyconsole/scripts/stubs/samples/hello-world.groovy
@@ -1,10 +1,20 @@
 import static com.github.dreamhead.moco.Moco.*
+import static com.cognifide.aem.stubs.moco.Moco.*
 
-stubs.server.get(by(uri("/hello-world"))).response("Hello! I am sample stub.")
+stubs.server
+        .get(by(uri("/hello-world")))
+        .response("Hello! I am sample stub.")
 
-stubs.server.get(
-        and(
-                by(uri("/secured")),
-                eq(query("password"), "secret")
+stubs.server
+        .get(
+                and(
+                        by(uri("/secured")),
+                        eq(query("password"), "secret")
+                )
         )
-).response("Secured endpoint revealed!")
+        .response("Secured endpoint revealed!")
+
+stubs.server
+        .get(by(uri("/current-date")))
+        .response(template('Today date is "${date}"', "date", dynamicVar({ new Date() })))
+
