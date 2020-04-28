ca74feeb48af18e930735f597fe787a37acce77c
Krystian Panek <krystian.panek@cognifide.com>
2020-04-28 13:57:47 +0200

    Minor

--- moco/src/main/java/com/cognifide/aem/stubs/moco/MocoStubs.java
+++ moco/src/main/java/com/cognifide/aem/stubs/moco/MocoStubs.java
@@ -100,7 +100,7 @@ public class MocoStubs extends AbstractStubs<HttpServer> {
     start();
   }
 
-  @ObjectClassDefinition(name = "AEM Stubs - Moco Server")
+  @ObjectClassDefinition(name = "AEM Stubs Moco Server")
   public @interface Config {
 
     @AttributeDefinition(name = "HTTP Server Port")
