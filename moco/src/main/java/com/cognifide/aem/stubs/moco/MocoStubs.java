24c1023c22357560eb1994a50bd6eb9350009612
Krystian Panek <krystian.panek@cognifide.com>
2020-04-28 13:52:40 +0200

    Config update / no bundle restart needed

--- moco/src/main/java/com/cognifide/aem/stubs/moco/MocoStubs.java
+++ moco/src/main/java/com/cognifide/aem/stubs/moco/MocoStubs.java
@@ -64,9 +64,14 @@ public class MocoStubs extends AbstractStubs<HttpServer> {
   }
 
   @Activate
-  @Modified
   protected void activate(Config config) {
     this.config = config;
+  }
+
+  @Modified
+  protected void modify(Config config) {
+    this.config = config;
+    reset();
   }
 
   @Deactivate
