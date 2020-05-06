da4bd4337f2dccd9045f09818c9a6f6203f4ffd2
Krystian Panek <krystian.panek@cognifide.com>
2020-05-06 08:47:01 +0200

    Tests impr

--- moco/build.gradle.kts
+++ moco/build.gradle.kts
@@ -18,39 +18,41 @@ dependencies {
 
 }
 
-aem {
-    tasks {
-        jar {
-            bundle {
-                attribute("DynamicImport-Package", "*")
-                importPackageSuffix.set("*;resolution:=optional")
-                importPackage("javax.annotation;version=0.0.0")
+tasks {
+    jar {
+        bundle {
+            attribute("DynamicImport-Package", "*")
+            importPackageSuffix.set("*;resolution:=optional")
+            importPackage("javax.annotation;version=0.0.0")
 
-                embedPackage("com.github.dreamhead:moco-core:1.1.0", "com.github.dreamhead.moco.*", export = true)
-                embedPackage("com.github.dreamhead:moco-runner:1.1.0")
-                embedPackage("com.google.guava:guava:28.2-jre", "com.google.common.*")
-                embedPackage("com.jayway.jsonpath:json-path:2.4.0",
-                        "com.jayway.jsonpath",
-                        "com.jayway.jsonpath.internal.*",
-                        "com.jayway.jsonpath.internal.filter",
-                        "com.jayway.jsonpath.internal.path",
-                        "com.jayway.jsonpath.spi.cache",
-                        "com.jayway.jsonpath.spi.json",
-                        "com.jayway.jsonpath.spi.mapper"
-                )
-            }
+            embedPackage("com.github.dreamhead:moco-core:1.1.0", "com.github.dreamhead.moco.*", export = true)
+            embedPackage("com.github.dreamhead:moco-runner:1.1.0")
+            embedPackage("com.google.guava:guava:28.2-jre", "com.google.common.*")
+            embedPackage("com.jayway.jsonpath:json-path:2.4.0",
+                    "com.jayway.jsonpath",
+                    "com.jayway.jsonpath.internal.*",
+                    "com.jayway.jsonpath.internal.filter",
+                    "com.jayway.jsonpath.internal.path",
+                    "com.jayway.jsonpath.spi.cache",
+                    "com.jayway.jsonpath.spi.json",
+                    "com.jayway.jsonpath.spi.mapper"
+            )
         }
+    }
 
-        packageCompose {
+    packageCompose {
+        installBundle("io.netty:netty-common:4.1.46.Final")
+        installBundle("io.netty:netty-handler:4.1.46.Final")
+        installBundle("io.netty:netty-buffer:4.1.46.Final")
+        installBundle("io.netty:netty-codec:4.1.46.Final")
+        installBundle("io.netty:netty-codec-http:4.1.46.Final")
+        installBundle("io.netty:netty-transport:4.1.46.Final")
+        installBundle("io.netty:netty-resolver:4.1.46.Final")
+        installBundle("org.apache.servicemix.bundles:org.apache.servicemix.bundles.freemarker:2.3.29_1")
+    }
 
-            installBundle("io.netty:netty-common:4.1.46.Final")
-            installBundle("io.netty:netty-handler:4.1.46.Final")
-            installBundle("io.netty:netty-buffer:4.1.46.Final")
-            installBundle("io.netty:netty-codec:4.1.46.Final")
-            installBundle("io.netty:netty-codec-http:4.1.46.Final")
-            installBundle("io.netty:netty-transport:4.1.46.Final")
-            installBundle("io.netty:netty-resolver:4.1.46.Final")
-            installBundle("org.apache.servicemix.bundles:org.apache.servicemix.bundles.freemarker:2.3.29_1")
-        }
+    integrationTest {
+        outputs.upToDateWhen { false }
+        testLogging.showStandardStreams = true
     }
 }
--- wiremock/build.gradle.kts
+++ wiremock/build.gradle.kts
@@ -17,88 +17,82 @@ dependencies {
     testImplementation("io.rest-assured:json-schema-validator:3.3.0")
 }
 
-aem {
-    tasks {
-        jar {
-            bundle {
-                attribute("DynamicImport-Package", "*")
-                importPackageSuffix.set("*;resolution:=optional")
-                importPackage("javax.annotation;version=0.0.0")
+tasks {
+    jar {
+        bundle {
+            attribute("DynamicImport-Package", "*")
+            importPackageSuffix.set("*;resolution:=optional")
+            importPackage("javax.annotation;version=0.0.0")
 
-                exportPackage("com.github.tomakehurst.wiremock.*")
+            exportPackage("com.github.tomakehurst.wiremock.*")
 
-                embedPackage("io.pebbletemplates:pebble:3.1.0", "com.mitchellbosecke.pebble.*")
-                embedPackage("org.unbescape:unbescape:1.0", "org.unbescape.*")
-                embedPackage("com.github.ben-manes.caffeine:caffeine:2.8.1",
-                        "com.github.benmanes.caffeine.cache.*",
-                        "com.github.benmanes.caffeine.base"
-                )
-                embedPackage("org.apache.commons:commons-lang3:3.9", "org.apache.commons.lang3.*")
-                embedPackage("org.apache.commons:commons-collections4:4.4", "org.apache.commons.collections4.*")
-                embedPackage("com.github.tomakehurst:wiremock:2.21.0", "com.github.tomakehurst.wiremock.*")
-                embedPackage("org.eclipse.jetty:jetty-servlets:9.4.20.v20190813", "org.eclipse.jetty.servlets")
-                embedPackage("com.google.guava:guava:27.0.1-jre",
-                        "com.google.common.base",
-                        "com.google.common.base.internal",
-                        "com.google.common.collect",
-                        "com.google.common.net",
-                        "com.google.common.escape",
-                        "com.google.common.hash",
-                        "com.google.common.io",
-                        "com.google.common.graph",
-                        "com.google.common.math",
-                        "com.google.common.primitives",
-                        "com.google.thirdparty.publicsuffix"
-                )
-                embedPackage("xmlunit:xmlunit:1.6",
-                        "org.custommonkey.xmlunit",
-                        "org.custommonkey.xmlunit.examples",
-                        "org.custommonkey.xmlunit.exceptions",
-                        "org.custommonkey.xmlunit.jaxp13",
-                        "org.custommonkey.xmlunit.util"
-                )
-                embedPackage("org.xmlunit:xmlunit-core:2.6.3", "org.xmlunit.*")
-                embedPackage("com.jayway.jsonpath:json-path:2.4.0",
-                        "com.jayway.jsonpath",
-                        "com.jayway.jsonpath.internal.*",
-                        "com.jayway.jsonpath.internal.filter",
-                        "com.jayway.jsonpath.internal.path",
-                        "com.jayway.jsonpath.spi.cache",
-                        "com.jayway.jsonpath.spi.json",
-                        "com.jayway.jsonpath.spi.mapper"
-                )
-                embedPackage("net.sf.jopt-simple:jopt-simple:5.0.4", "joptsimple.*")
-                embedPackage("com.github.jknack:handlebars:4.1.2",
-                        "com.github.jknack.handlebars",
-                        "com.github.jknack.handlebars.cache",
-                        "com.github.jknack.handlebars.context",
-                        "com.github.jknack.handlebars.io",
-                        "com.github.jknack.handlebars.internal.*",
-                        "com.github.jknack.handlebars.helper"
-                )
-                embedPackage("org.apache.tapestry:tapestry-json:5.3.7", "org.codehaus.jettison.*")
-                embedPackage("org.codehaus.jettison:jettison:1.4.0", "org.apache.tapestry5.json")
-                embedPackage("net.minidev:json-smart:2.3",
-                        "net.minidev.json",
-                        "net.minidev.json.parser",
-                        "net.minidev.asm",
-                        "net.minidev.json.annotate",
-                        "net.minidev.json.reader",
-                        "net.minidev.asm.ex",
-                        "net.minidev.json.writer"
-                )
-                embedPackage("org.ow2.asm:asm:7.0", "org.objectweb.asm")
-                embedPackage("com.flipkart.zjsonpatch:zjsonpatch:0.4.9", "com.flipkart.zjsonpatch")
-            }
+            embedPackage("io.pebbletemplates:pebble:3.1.0", "com.mitchellbosecke.pebble.*")
+            embedPackage("org.unbescape:unbescape:1.0", "org.unbescape.*")
+            embedPackage("com.github.ben-manes.caffeine:caffeine:2.8.1",
+                    "com.github.benmanes.caffeine.cache.*",
+                    "com.github.benmanes.caffeine.base"
+            )
+            embedPackage("org.apache.commons:commons-lang3:3.9", "org.apache.commons.lang3.*")
+            embedPackage("org.apache.commons:commons-collections4:4.4", "org.apache.commons.collections4.*")
+            embedPackage("com.github.tomakehurst:wiremock:2.21.0", "com.github.tomakehurst.wiremock.*")
+            embedPackage("org.eclipse.jetty:jetty-servlets:9.4.20.v20190813", "org.eclipse.jetty.servlets")
+            embedPackage("com.google.guava:guava:27.0.1-jre",
+                    "com.google.common.base",
+                    "com.google.common.base.internal",
+                    "com.google.common.collect",
+                    "com.google.common.net",
+                    "com.google.common.escape",
+                    "com.google.common.hash",
+                    "com.google.common.io",
+                    "com.google.common.graph",
+                    "com.google.common.math",
+                    "com.google.common.primitives",
+                    "com.google.thirdparty.publicsuffix"
+            )
+            embedPackage("xmlunit:xmlunit:1.6",
+                    "org.custommonkey.xmlunit",
+                    "org.custommonkey.xmlunit.examples",
+                    "org.custommonkey.xmlunit.exceptions",
+                    "org.custommonkey.xmlunit.jaxp13",
+                    "org.custommonkey.xmlunit.util"
+            )
+            embedPackage("org.xmlunit:xmlunit-core:2.6.3", "org.xmlunit.*")
+            embedPackage("com.jayway.jsonpath:json-path:2.4.0",
+                    "com.jayway.jsonpath",
+                    "com.jayway.jsonpath.internal.*",
+                    "com.jayway.jsonpath.internal.filter",
+                    "com.jayway.jsonpath.internal.path",
+                    "com.jayway.jsonpath.spi.cache",
+                    "com.jayway.jsonpath.spi.json",
+                    "com.jayway.jsonpath.spi.mapper"
+            )
+            embedPackage("net.sf.jopt-simple:jopt-simple:5.0.4", "joptsimple.*")
+            embedPackage("com.github.jknack:handlebars:4.1.2",
+                    "com.github.jknack.handlebars",
+                    "com.github.jknack.handlebars.cache",
+                    "com.github.jknack.handlebars.context",
+                    "com.github.jknack.handlebars.io",
+                    "com.github.jknack.handlebars.internal.*",
+                    "com.github.jknack.handlebars.helper"
+            )
+            embedPackage("org.apache.tapestry:tapestry-json:5.3.7", "org.codehaus.jettison.*")
+            embedPackage("org.codehaus.jettison:jettison:1.4.0", "org.apache.tapestry5.json")
+            embedPackage("net.minidev:json-smart:2.3",
+                    "net.minidev.json",
+                    "net.minidev.json.parser",
+                    "net.minidev.asm",
+                    "net.minidev.json.annotate",
+                    "net.minidev.json.reader",
+                    "net.minidev.asm.ex",
+                    "net.minidev.json.writer"
+            )
+            embedPackage("org.ow2.asm:asm:7.0", "org.objectweb.asm")
+            embedPackage("com.flipkart.zjsonpatch:zjsonpatch:0.4.9", "com.flipkart.zjsonpatch")
         }
     }
-}
 
-tasks {
-    pmdMain {
-        enabled = true
-    }
-    pmdIntegTest {
-        enabled = false
+    integrationTest {
+        outputs.upToDateWhen { false }
+        testLogging.showStandardStreams = true
     }
 }
