f6de78e53e0335c27c6aaa1fa1733facf462076a
Krystian Panek <krystian.panek@cognifide.com>
2020-05-08 11:54:00 +0200

    Doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ apply(from = "gradle/fork/props.gradle.kts")
 apply(from = "gradle/common.gradle.kts")
 
 description = "AEM Stubs"
-defaultTasks(":assembly:full:packageDeploy")
+defaultTasks(":assembly:all:packageDeploy")
 
 aem {
     instance {
