702eb0303da490c3840acbfe6fb81536f85d861a
Krystian Panek <krystian.panek@cognifide.com>
2020-05-28 10:14:49 +0200

    Filter bypass for AEM <= 6.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:14.0.0-beta")
+    implementation("com.cognifide.gradle:aem-plugin:14.0.0")
     implementation("com.neva.gradle:fork-plugin:5.0.1")
     implementation("net.researchgate:gradle-release:2.6.0")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
--- wiremock/src/main/java/com/cognifide/aem/stubs/wiremock/servlet/WireMockFilter.java
+++ wiremock/src/main/java/com/cognifide/aem/stubs/wiremock/servlet/WireMockFilter.java
@@ -4,20 +4,23 @@ import com.cognifide.aem.stubs.wiremock.WireMockStubs;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.http.whiteboard.Preprocessor;
 
 import javax.servlet.*;
 import java.io.IOException;
 
 /**
- * Bypass for AEM >= 6.4.x for avoiding AEM built-in filters intercepting calls to WireMock Servlet unnecessarily.
+ * Bypass for AEM <= 6.3.x for avoiding AEM built-in filters intercepting calls to WireMock Servlet unnecessarily.
  */
 @Component(
   immediate = true,
-  service = Preprocessor.class,
-  property = Constants.SERVICE_RANKING + ":Integer=" + Integer.MAX_VALUE
+  service = Filter.class,
+  property = {
+    Constants.SERVICE_RANKING + ":Integer=" + Integer.MAX_VALUE,
+    "osgi.http.whiteboard.filter.pattern=/",
+    "osgi.http.whiteboard.context.select=(osgi.http.whiteboard.context.name=*)"
+  }
 )
-public class WireMockPreprocessor implements Preprocessor {
+public class WireMockFilter implements Filter {
 
   @Reference
   private WireMockStubs stubs;
@@ -25,14 +28,14 @@ public class WireMockPreprocessor implements Preprocessor {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     if (stubs.isBypassable(request)) {
-        stubs.getServlet().service(request, response);
+      stubs.getServlet().service(request, response);
     } else {
       chain.doFilter(request, response);
     }
   }
 
   @Override
-  public void init(FilterConfig filterConfig) {
+  public void init(FilterConfig filterConfig) throws ServletException {
     // intentionally empty
   }
 
