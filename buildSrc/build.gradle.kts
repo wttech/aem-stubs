d28916b0bb5da8849fe580146881a63f6101b6d9
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2020-12-01 08:49:55 +0100

    Upgrade GAP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:14.4.13")
+    implementation("com.cognifide.gradle:aem-plugin:14.4.24")
     implementation("com.neva.gradle:fork-plugin:5.0.13")
     implementation("net.researchgate:gradle-release:2.6.0")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
