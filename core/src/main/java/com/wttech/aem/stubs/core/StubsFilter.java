a8d0932bef29997d88845cb2ad267818ebad7bd5
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-06-25 09:21:52 +0200

    Buildable & configurable

--- core/src/main/java/com/wttech/aem/stubs/core/StubsFilter.java
+++ core/src/main/java/com/wttech/aem/stubs/core/StubsFilter.java
@@ -1,7 +1,13 @@
-package com.wttech.aem.stubs.core.filters;
+package com.wttech.aem.stubs.core;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +19,32 @@ import java.io.IOException;
 @Component(
         service = Filter.class,
         property = {
-                HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_REGEX + "=/stubs/[a-z]*",
                 HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT + "=(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=*)"
-        }
+        },
+        configurationPolicy = ConfigurationPolicy.REQUIRE
 )
+@Designate(ocd = StubsFilter.Config.class)
 public class StubsFilter implements Filter {
+
     private static final Logger LOG = LoggerFactory.getLogger(StubsFilter.class);
+
+    private Config config;
+
+    @ObjectClassDefinition(name = "AEM Stubs - HTTP Filter")
+    public @interface Config {
+
+        @AttributeDefinition(name = "Enabled")
+        boolean enabled();
+
+        @AttributeDefinition(name = "Whiteboard Filter Regex")
+        String[] osgi_http_whiteboard_filter_regex();
+    }
+
+    @Activate
+    @Modified
+    protected void activate(Config config) {
+        this.config = config;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
