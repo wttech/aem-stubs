62560ca258a75f443e0c676aa7e58ef99619674a
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-06-25 08:43:49 +0200

    Filter

--- core/src/main/java/com/wttech/aem/stubs/core/StubsFilter.java
+++ core/src/main/java/com/wttech/aem/stubs/core/StubsFilter.java
@@ -1,38 +1,47 @@
 package com.wttech.aem.stubs.core.filters;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardFilterPattern;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component(service = Filter.class)
-@HttpWhiteboardFilterPattern("/stubs/*")
+@Component(
+        service = Filter.class,
+        property = {
+                HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_REGEX + "=/stubs/[a-z]*",
+                HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT + "=(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=*)"
+        }
+)
 public class StubsFilter implements Filter {
-
     private static final Logger LOG = LoggerFactory.getLogger(StubsFilter.class);
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Initialization code here
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().write("{\"message\": \"Hello World!\"}");
+
+
+       // LOG.info("Stubs | Hello World!");
+
+       // chain.doFilter(request, response);
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        // Pre-processing code here
-
-        LOG.info("Stubs - Hello world!");
-
-        chain.doFilter(request, response);
-
-        // Post-processing code here
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
     }
 
     @Override
     public void destroy() {
-        // Cleanup code here
+        // do nothing
     }
 }
