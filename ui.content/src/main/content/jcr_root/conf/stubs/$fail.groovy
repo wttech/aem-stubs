a72e091ba830af9ea4ba9cca59cbae57a2b8d663
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-07-18 10:24:16 +0200

    Perfect troubleshooting

--- ui.content/src/main/content/jcr_root/conf/stubs/$fail.groovy
+++ ui.content/src/main/content/jcr_root/conf/stubs/$fail.groovy
@@ -1,8 +1,13 @@
+import org.apache.commons.lang3.exception.ExceptionUtils
+
 void fail(HttpServletRequest request, HttpServletResponse response, Exception exception) {
     response.setStatus(500)
     template.render(response, "/conf/stubs/\$fail.html", [
             "request": request,
             "response": response,
             "exception": exception,
+
+            "stackTrace": ExceptionUtils.getStackTrace(exception),
+            "rootCause": ExceptionUtils.getRootCause(exception),
     ])
 }
